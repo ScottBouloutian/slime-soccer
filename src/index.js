fb26d0334045ca5c49c2336a89db3522587a57be
Scott Bouloutian <ScottBouloutian@gmail.com>
2018-01-28 11:19:02 -0500

    added initial jumping mechanics

--- src/index.js
+++ src/index.js
@@ -5,12 +5,16 @@ import Phaser from 'expose-loader?Phaser!phaser-ce/build/custom/phaser-split.js'
 const worldHeight = 400;
 const worldWidth = 560;
 const worldPadding = 20;
-
+const jumpHeight = 57;
+const slimeHeight = 28;
 const game = new Phaser.Game(worldWidth, worldHeight, Phaser.AUTO, 'slime-soccer', {
     create: create,
     preload: preload,
     update: update,
 });
+
+var slime = null;
+var cursors = null;
 
 function preload() {
     game.load.image('background', './assets/images/background.png');
@@ -19,7 +23,8 @@ function preload() {
 
 function create() {
     game.physics.startSystem(Phaser.Physics.BOX2D);
-    game.physics.box2d.gravity.y = 500;
+    game.physics.box2d.gravity.y = 40;
+    cursors = game.input.keyboard.createCursorKeys();
 
     // Add the background to the world
     game.world.setBounds(-worldPadding, -worldPadding, worldWidth - 2 * worldPadding, worldHeight - 2 * worldPadding);
@@ -27,10 +32,20 @@ function create() {
     game.add.image(-worldPadding, -worldPadding, 'background');
 
     // Add the slime to the world
-    const slime = game.add.sprite(100, 100, 'slime');
+    slime = game.add.sprite(worldPadding, worldHeight - worldPadding - slimeHeight, 'slime');
     game.physics.box2d.enable(slime);
 }
 
 function update() {
+    const distanceFromGround = worldHeight - slime.y - worldPadding - slimeHeight;
+
+    // Jumping
+    if (distanceFromGround > jumpHeight) {
+        slime.body.velocity.y = 40;
+    }
+    if (cursors.up.isDown && distanceFromGround < 8) {
+        slime.body.moveUp(150);
+    }
+
     game.debug.box2dWorld();
 }
